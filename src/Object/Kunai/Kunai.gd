1342ef5aa051c546976a7d254580b13f8e8f7908
aditgocendra <gocendra123@gmail.com>
2020-08-21 18:07:11 +0700

    add new enemy

--- src/Object/Kunai/Kunai.gd
+++ src/Object/Kunai/Kunai.gd
@@ -8,7 +8,7 @@ func _on_FreeTime_timeout():
 
 
 func _on_Kunai_body_entered(body):
-	if body is Bandit:
+	if body is Bandit or ReaperMan:
 #		var direct_sprite = -1 if self.linear_velocity.x > 0 else 1
 		body.enemy_dead()
 		queue_free()
